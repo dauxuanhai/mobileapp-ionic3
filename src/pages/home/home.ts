ebfd61c7d059af437363ab89d0c1f530f396e5e1
Dung Le <phieudu241@gmail.com>
2017-11-16 08:50:11 +0700

    Integrate ngx-translate to app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    lang: string = 'en';
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController,
+                public translate: TranslateService) {
 
-  }
+    }
 
+    switchLanguage(lang) {
+        this.lang = (lang == 'en') ? 'ar' : 'en';
+        this.translate.use(this.lang);
+    }
 }
